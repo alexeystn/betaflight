3e479f62a941722515409193529725ea5532ebb7
Alexey Stankevich <alexey.stn@gmail.com>
2020-12-06 22:09:28 +0300

    Implement Level Recovery for 4.2.5

--- src/main/osd/osd_elements.c
+++ src/main/osd/osd_elements.c
@@ -1431,6 +1431,13 @@ static void osdElementWarnings(osdElementParms_t *element)
         return;
     }
 
+    if (isLevelRecoveryActive() && osdWarnGetState(OSD_WARNING_LEVEL_RECOVERY)) {
+        tfp_sprintf(element->buff, "RECOVERY");
+        element->attr = DISPLAYPORT_ATTR_WARNING;
+        SET_BLINK(OSD_WARNINGS);
+        return;
+    }
+
 #ifdef USE_ADC_INTERNAL
     const int16_t coreTemperature = getCoreTemperatureCelsius();
     if (osdWarnGetState(OSD_WARNING_CORE_TEMPERATURE) && coreTemperature >= osdConfig()->core_temp_alarm) {
